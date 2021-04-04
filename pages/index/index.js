9a737cd6523706d745a3b47e7ed201a5259663df
leibaio <193430727@qq.com>
2021-04-05 00:16:29 +0800

    1.0.2
    加入开场动画，优化界面

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,6 +6,8 @@ Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
+    remind: '加载中',
+    angle: 0,
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
@@ -68,5 +70,26 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+  onReady: function() {
+    var that = this;
+    setTimeout(function() {
+      that.setData({
+        remind: ''
+      });
+    }, 2000);
+    wx.onAccelerometerChange(function(res) {
+      var angle = -(res.x * 30).toFixed(1);
+      if (angle > 14) {
+        angle = 14;
+      } else if (angle < -14) {
+        angle = -14;
+      }
+      if (that.data.angle !== angle) {
+        that.setData({
+          angle: angle
+        });
+      }
+    });
   }
 })
